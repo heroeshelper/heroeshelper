f51abced08512782429f9eb934350fd812493937
Sepherane <bob.vankamp@gmail.com>
2024-10-06 14:10:21 +0200

    update lineup layout

--- src/app/heroes/line-up/page.tsx
+++ src/app/heroes/line-up/page.tsx
@@ -5,10 +5,21 @@ import HeroCard from "@shared/heroes/HeroCard";
 const HeroesLineup = () => {
   const heroes = useHeroes();
   return (
-    <div>
-      {heroes.map((x) => (
-        <HeroCard key={x.shortname} hero={x} />
-      ))}
+    <div className="flex flex-col items-center gap-2 mt-4">
+      <div className="flex flex-row items-center gap-2">
+        <HeroCard />
+        <HeroCard />
+      </div>
+      <div className="flex flex-row items-center gap-2">
+        <HeroCard />
+        <HeroCard />
+        <HeroCard />
+      </div>
+      <div className="flex flex-row items-center gap-2 mt-4">
+        {heroes.map((x) => (
+          <HeroCard key={x.shortname} hero={x} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/shared/heroes/HeroCard.tsx
+++ src/shared/heroes/HeroCard.tsx
@@ -2,18 +2,20 @@ import { Hero } from "./types";
 import Image from "next/image";
 
 type Props = {
-  hero: Hero;
+  hero?: Hero;
 };
 
 const HeroCard = ({ hero }: Props) => {
   return (
-    <div className={`herocard ${hero.rarity}`}>
-      <Image
-        src={`/assets/heroes/${hero.shortname}.png`}
-        alt={hero.shortname}
-        width={216}
-        height={260}
-      />
+    <div className={`herocard ${hero && hero.rarity}`}>
+      {hero && (
+        <Image
+          src={`/assets/heroes/${hero.shortname}.png`}
+          alt={hero.shortname}
+          width={216}
+          height={260}
+        />
+      )}
     </div>
   );
 };
