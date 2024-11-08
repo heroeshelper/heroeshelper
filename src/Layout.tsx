6e7e0bb8d8b72d2bf90fec0f2294970ce45f1ceb
Sepherane <bob.vankamp@gmail.com>
2024-11-08 22:05:10 +0100

    Add footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import TopHeader from "./shared/components/TopHeader";
+import Footer from "./shared/components/Footer";
 
 const Layout = () => {
     return (
         <>
             <TopHeader />
             <Outlet />
+            <Footer />
         </>
     );
 };
