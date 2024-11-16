f754e9011aca940a2a6e607399ea120a252b50ca
Sepherane <bob.vankamp@gmail.com>
2024-11-16 10:32:53 +0100

    Add webp support

--- src/shared/components/Image.tsx
+++ src/shared/components/Image.tsx
@@ -1,5 +1,11 @@
 const Image = (props: React.ComponentProps<"img">) => {
-    return <img loading="lazy" {...props}></img>;
+    const allowWebP = props.src?.endsWith(".png");
+    return (
+        <picture>
+            {allowWebP && <source type="image/webp" srcSet={props.src?.replace(".png", ".webp")} />}
+            <img alt={props.alt ?? ""} loading="lazy" {...props} />
+        </picture>
+    );
 };
 
 export default Image;
