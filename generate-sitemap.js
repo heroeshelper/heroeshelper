e258d92fa3f2ecb50b351d63f73f0986fcf24727
Sepherane <bob.vankamp@gmail.com>
2024-11-17 17:29:43 +0100

    Add sitemap generator

---
+++ generate-sitemap.js
@@ -0,0 +1,20 @@
+import fs from "node:fs"
+
+const file = "./public/sitemap.txt";
+const baseUrl = "https://heroeshelper.net"
+
+const urls = [
+    "",
+    "/heroes/list",
+    "/heroes/line-up",
+    "/about",
+    "/cookies"
+]
+
+const heroes = JSON.parse(fs.readFileSync("./src/shared/heroes/data/heroes.json"));
+
+heroes.forEach(hero => urls.push(`/hero/${hero.id}-${hero.shortname}`));
+
+fs.writeFileSync(file, urls.map(u => `${baseUrl}${u}`).join("\n"));
+
+console.log(`Successfully generated sitemap with ${urls.length} urls.`);
