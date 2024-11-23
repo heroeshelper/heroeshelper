bf2049daf36a7874135dd52f87006e191ffe1a88
Sepherane <bob.vankamp@gmail.com>
2024-11-23 12:22:53 +0100

    Add app manifest

--- netlify/plugins/favicon-downloader/index.js
+++ netlify/plugins/favicon-downloader/index.js
@@ -4,12 +4,23 @@ import fs from "node:fs"
 
 const publicDir = "./public"
 
+const files = [
+    "favicon.ico",
+    "apple-touch-icon.png",
+    "favicon-16x16.png",
+    "favicon-32x32.png",
+    "favicon-192x192.png",
+    "favicon-512x512.png"
+]
+
 export const onPreBuild = async function () {
     if (!fs.existsSync(publicDir)) {
         fs.mkdirSync(publicDir);
     }
 
-    const response = await fetch(`${process.env.VITE_ASSET_LOCATION}/favicon.ico`)
-    const stream = Readable.fromWeb(response.body)
-    await writeFile(`${publicDir}/favicon.ico`, stream)
+    for (const fileName of files) {
+        const response = await fetch(`${process.env.VITE_ASSET_LOCATION}/${fileName}`);
+        const stream = Readable.fromWeb(response.body);
+        await writeFile(`${publicDir}/${fileName}`, stream);
+    }
 }
